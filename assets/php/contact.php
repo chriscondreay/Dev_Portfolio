0d9f4670ab1fb036d2b5d1ffc31383f16d03edac
The Instigator <condreaychris@gmail.com>
2022-07-24 03:46:30 -0700

    testing php contact form

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -5,23 +5,33 @@
   $phone = $_POST['phone'];
   $message = $_POST['message'];
 
-  $email_from = "condreaychris@gmail.com";
+  $mailHeader = "From:".$name."<".$email.">\r\n";
 
-  $email_subject = "New Form Submission";
+  $recipient = "condreaychris@gmail.com";
 
-  $email_body = "Name: $name \n".
-  "Email: $email \n".
-  "Phone: $phone \n".
-  "Message: $message".
+  mail($recipient, $name, $email, $phone, $message, $mailHeader) or die("Error!");
 
-  $to = "condreaychris@gmail.com";
+  echo `
+  
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/52a67c9491.js" crossorigin="anonymous"></script> 
+    <link rel="stylesheet" href="./assets/styles/style.css">
+    <title>Chris Condreay</title>
+  </head>
+  <body>
 
-  $headers = "From: $email_from \r\n";
+    <h1>Thank you!</h1>
+    <p>I'll be sure to get back to as soon as possible!</p>
+    <p>Head back to <a href="./index.html">homepage</a></p>
 
-  $headers .= "Reply-To: $email \r\n";
-
-  mail($to, $email_subject, $email_body, $header)
-
-  header("Location: index.html");
+  </body>
+  </html>
+  
+  `;
 
 ?>
