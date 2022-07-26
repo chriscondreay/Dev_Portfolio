a7ad9bed16f66aa49c65d0658621a4353330cd77
The Instigator <condreaychris@gmail.com>
2022-07-25 17:33:46 -0700

    styled headers and contact

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,8 @@ let email = document.querySelector('#email');
 let phone = document.querySelector('#phone');
 let message = document.querySelector('#message');
 
+// Send contact info event listener
+
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
@@ -33,3 +35,42 @@ contactForm.addEventListener('submit', (e) => {
   xhr.send(JSON.stringify(formData));
 
 });
+
+// Navbar hide and reveal
+let didScroll;
+let lastScrollTop = 0;
+let delta = 5;
+let navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(e) {
+  didScroll = true;
+})
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 10);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
