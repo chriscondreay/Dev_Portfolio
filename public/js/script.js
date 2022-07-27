3655e74945bb15eabad94652db9bd013fda65b7f
Chris <condreaychris@gmail.com>
2022-07-26 20:10:37 -0700

    fixed card issues and styled

--- public/js/script.js
+++ public/js/script.js
@@ -1,40 +1,40 @@
-// const contactForm = document.querySelector('.contact-form')
-// let name = document.querySelector('#name');
-// let email = document.querySelector('#email');
-// let phone = document.querySelector('#phone');
-// let message = document.querySelector('#message');
+const contactForm = document.querySelector('.contact-form')
+let name = document.querySelector('#name');
+let email = document.querySelector('#email');
+let phone = document.querySelector('#phone');
+let message = document.querySelector('#message');
 
-// // Send contact info event listener
+// Send contact info event listener
 
-// contactForm.addEventListener('submit', (e) => {
-//   e.preventDefault();
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-//   let formData = {
-//     name: name.value,
-//     email: email.value,
-//     phone: phone.value,
-//     message: message.value,
-//   }
+  let formData = {
+    name: name.value,
+    email: email.value,
+    phone: phone.value,
+    message: message.value,
+  }
 
-//   let xhr = new XMLHttpRequest();
-//   xhr.open('POST', '/');
-//   xhr.setRequestHeader('content-type', 'application/json');
-//   xhr.onload = function() {
-//     console.log(xhr.responseText);
-//     if (xhr.responseText == 'success') {
-//       alert('Email was sent!');
-//       name.value = '';
-//       email.value = '';
-//       phone.value = '';
-//       message.value = '';
-//     } else {
-//       alert('Email not sent')
-//     }
-//   }
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.onload = function() {
+    console.log(xhr.responseText);
+    if (xhr.responseText == 'success') {
+      alert('Email was sent!');
+      name.value = '';
+      email.value = '';
+      phone.value = '';
+      message.value = '';
+    } else {
+      alert('Email not sent')
+    }
+  }
 
-//   xhr.send(JSON.stringify(formData));
+  xhr.send(JSON.stringify(formData));
 
-// });
+});
 
 // Navbar hide and reveal
 
